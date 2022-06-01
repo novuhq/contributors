cc43bf15cdd04d906f5f11a0716b3c3f464ce7b6
Nevo David <nevo@notu.co>
2022-06-01 13:19:46 +0700

    Feat(): fix

--- packages/api/src/app/contributor.service.ts
+++ packages/api/src/app/contributor.service.ts
@@ -19,7 +19,7 @@ export class ContributorService {
     ]).exec();
 
     return {
-      list: list.map(({ pulls, ...params }) => params),
+      list: list.map(({ pulls, last3MonthsPulls, ...params }) => params),
       pages: Math.ceil(+pages[0].pages / 30),
     };
   }
